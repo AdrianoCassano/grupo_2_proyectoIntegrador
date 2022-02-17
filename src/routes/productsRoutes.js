6c2c08f36618eac45143ce59c9a533437ad83695
AdrianoCassano <cassano.adriano@gmail.com>
2022-02-16 22:51:26 -0300

    Multer funcionando

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -6,7 +6,7 @@ const productsController = require("../controllers/productsController");
 const multer = require ("multer");
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null, path.join(__dirname, "../public/images/products"));
+        cb(null, "./public/images/products");
     },
     filename: function (req,file,cb){
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
@@ -14,16 +14,16 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage});
-router.post('/products/create', upload.single('productImg'), (req, res) => {
+router.post('/products', upload.single('productImg'), (req, res) => {
     console.log(req.file) 
     res.send('Archivo subido correctamente')
   })
 
 router.get("/", productsController.products);
 router.get("/creacion", productsController.creacion);
+router.post("/",upload.single("productImg"), productsController.creado);
 router.get("/:id", productsController.detalle);
 
-router.post("/",upload.single("productImg"), productsController.creado);
 
 router.get("/:id/edicion", productsController.edicion);
 router.put("/:id",upload.single("productImg"), productsController.editado);
