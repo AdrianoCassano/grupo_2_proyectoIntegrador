0d269b38a0cf07d2ac80191843109d87d1952172
Restair6 <renzo.restaino@gmail.com>
2022-02-28 17:35:08 -0300

    Fix menor en rutas y base ejs de /products

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -6,7 +6,7 @@ const productsController = require("../controllers/productsController");
 const multer = require ("multer");
 const storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null, "./public/images/products");
+        cb(null, "public/images/products");
     },
     filename: function (req,file,cb){
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
@@ -22,10 +22,6 @@ router.post('/products', upload.single('productImg'), (req, res) => {
 router.get("/", productsController.products);
 router.get("/creacion", productsController.creacion);
 router.post("/",upload.single("productImg"), productsController.creado);
-<<<<<<< HEAD
-
-=======
->>>>>>> 6c2c08f36618eac45143ce59c9a533437ad83695
 router.get("/:id", productsController.detalle);
 
 
