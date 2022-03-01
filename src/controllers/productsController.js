512054c9d88b1fd245c959e159b2baf702b48444
Alvaro Blanquicett <alvaroblanquicett@gmail.com>
2022-02-28 20:40:03 -0500

    edicion ejs y product controller edicion

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -32,10 +32,23 @@ const productsController = {
         res.redirect("/products")
     },
     edicion: (req,res) => {
-        res.render("products/edicion")
+        let idProducto = req.params.id
+
+        /* res.render("products/edicion") */
+        res.send(idProducto)
     },
     editado: (req,res) => {
-        res.render("products/edicion")
+        let productoEditado = {
+            // id: hay que definir como hacemos,
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            // materiales: req.body.materiales,
+            precio: req.body.precio,
+            // dimensiones: req.body.dimensiones,
+            // peso: req.body.peso,
+            //imagen: req.file.imagen,
+        }
+        res.redirect("products/edicion")
     },
     destroy: (req,res) => {
         res.render("products/edicion")
