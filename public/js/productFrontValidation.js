38aae0a6bfa72165d886dd6d61bd3d8b4242c5f8
Restair6 <renzo.restaino@gmail.com>
2022-05-13 21:51:06 -0300

    front validation completa

--- public/js/productFrontValidation.js
+++ public/js/productFrontValidation.js
@@ -1,5 +1,57 @@
+console.log("ES POR ACA")
 window.addEventListener('load', function (){
 
+    let form = document.querySelector('.register-form');
+
+    form.addEventListener('submit', function(e){
+       
+        
+        let errores = [];
+        
+        let campoNombre = document.querySelector('#nombre');
+        
+        if(campoNombre.value ==""){
+            errores.push('El nombre del producto es obligatorio')  
+            
+        }else if (campoNombre.value.length < 6){
+            errores.push ('El nombre debe tener mínimo 5 caracteres')
+        }
+        
+        let campoDescripcion = document.querySelector('#descripcion');
+        
+        if(campoDescripcion.value ==""){
+            errores.push('la descripcion del producto es obligatoria')
+        }else if (campoDescripcion.value.length < 21){
+            errores.push ('La descripcion debe tener mínimo 20 caracteres');    
+        }
+
+        let image = document.querySelector('#productImg');
+
+        if(image.value.length == 0){
+            errores.push('Debe cargar una imagen')   
+        } 
+        else if (image.value) {
+            
+            fileName = image.value;
+            idxDot = fileName.lastIndexOf(".") + 1,
+            extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+            if (!(extFile == "jpg" || extFile == "jpeg" || extFile == "png")){
+                errores.push('Debe cargar una imagen en formato jpg/jpeg/png')
+            }
+        };
+
+        if (errores.length >0){
+            e.preventDefault();
+            let  ulErrores = document.querySelector("div.errores ul");
+            ulErrores.innerHTML= "";
+            for (let i = 0; i < errores.length; i++) { 
+                ulErrores.innerHTML +="<li>" + errores[i]+ "</li>"
+            }
+        }else{
+            form.submit();
+        }
+        
+});
 
 
 
--- public/js/registerFrontValidation.js
+++ public/js/registerFrontValidation.js
@@ -1,7 +1,85 @@
 window.addEventListener('load', function (){
 
+    let form = document.querySelector('.register-form');
+
+    form.addEventListener('submit', function(e){
+       
+        
+        let errores = [];
+      
+
+        let campoNombre = document.querySelector('#nombre');
+
+        if(campoNombre.value ==""){
+            errores.push('El nombre es obligatorio')
+                  
+        }else if (campoNombre.value.length < 2){
+            errores.push ('El nombre debe tener mínimo 2 caracteres')
+        }
+
+        let campoApellido = document.querySelector('#Apellido');
+
+        if(campoApellido.value ==""){
+            errores.push('El apellido es obligatorio')      
+        }else if (campoApellido.value.length < 2){
+            errores.push ('El apellido debe tener mínimo 2 caracteres')
+        }
+
+        
+
+        let campoEmail = document.querySelector('#Email');
+        
+        let regexEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/
 
 
+       
+        if(campoEmail.value=" "){
+            errores.push('El email es  obligatorio')  
 
+        }
+        else if (!regexEmail.test(campoEmail.value)) {
+            errores.push('El formato no es del tipo email')
+            return false 
+
+
+        }
     
+
+
+
+        
+        
+        let campoPassword = document.querySelector('#Contraseña');
+
+        if(campoPassword.value ==""){
+            errores.push('El Password es obligatorio')      
+        }else if (campoPassword.value.length < 9){
+            errores.push ('El Password debe tener mínimo 9 caracteres')
+        }
+
+        let image = document.querySelector('#file');
+
+        if (image.value) {
+            
+            fileName = image.value;
+            idxDot = fileName.lastIndexOf(".") + 1,
+            extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+            if (!(extFile == "jpg" || extFile == "jpeg" || extFile == "png")){
+                errores.push('Debe cargar una imagen en formato jpg/jpeg/png')
+            }
+        };
+
+
+        if (errores.length >0){
+            e.preventDefault();
+            let  ulErrores = document.querySelector("div.errores ul");
+            ulErrores.innerHTML= "";
+            for (let i = 0; i < errores.length; i++) { 
+                ulErrores.innerHTML +="<li>" + errores[i]+ "</li>"
+            }
+        }else{
+            form.submit();
+        }
+        
+})
 })
