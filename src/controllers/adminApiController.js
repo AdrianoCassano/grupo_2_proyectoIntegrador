615538441ca44ffe15f7f385a1500e634689b464
AdrianoCassano <cassano.adriano@gmail.com>
2022-05-15 00:54:53 -0300

    Api terminadas, editado products.ejs y detail.ejs

--- src/controllers/adminApiController.js
+++ src/controllers/adminApiController.js
@@ -1,4 +1,5 @@
 const db = require ("../database/models")
+const { Sequelize } = require('sequelize');
 
 const adminApiController ={
 
@@ -6,89 +7,123 @@ const adminApiController ={
         try {
             
         } catch (error) {
+            console.log(error)
             res.status(500).json({msg:"error"})
         }
         res.json("panel")
     },
     users: async (req,res) => {
         try {
+            const limit = 10
+            const page = parseInt(req.query.page)
+            const offset = page ? (page-1)* limit : 0
             let usersList = await db.User.findAndCountAll({
                 attributes: ["id", "firstName", "lastName", "email"],
                 include: {association:"usercategory"},
-                offset: 10,
-                limit: 10,
+                offset: offset,
+                limit: limit,
             })
 
+            const rows = usersList.rows.map(user => {
+                const { dataValues } = user;
+                const url = `${req.protocol}://${req.get('host')}${req.originalUrl}/${user.id}`;
+                return {
+                    ...dataValues,
+                    url
+                }
+            })
+            const totalPages = Math.ceil(usersList.count / limit)
+
             let response = {
-                status: 200,
+                totalPages: totalPages,
+                currentPageNumber: page,
                 count: usersList.count,
-                users: usersList.rows,
+                users: rows,
             }
             res.status(200).json({response})     
         } catch (error) {
+            console.log(error)
             res.status(500).json({msg:"error"})
         }
     },
     userDetail: async (req,res) => {
         try {
-            let userDetail = await db.User.findByPk(req.params.id,{attributes: {exclude:["password", "categoryId"]}})
-
+            let userDetail = await db.User.findByPk(req.params.id, {attributes: {exclude:["password", "categoryId"]}})
+            let userAvatarUrl = JSON.parse(userDetail.userAvatar)
+            
             let response = {
-                status: 200,
-                user: userDetail
+                user: userDetail,
+                userAvatarUrl: `${req.protocol}://${req.get('host')}${req.originalUrl}/${userAvatarUrl}`
             }
             res.status(200).json({response})     
         } catch (error) {
+            console.log(error)
             res.status(500).json({msg:"error"})
         }
     },
     products: async (req,res) => {
         try {
-            let  products = await db.Product.findAndCountAll({
-                include: [{association:"categorias"},{association:"users"}]
+            const limit = 10
+            const page = req.query.page
+            const offset = page ? (page-1)* limit : 0
+            let products = await db.Product.findAndCountAll({
+                attributes: ["id", "nombre", "descripcion"],
+                include: [{association:"categorias"},{association:"users"}],
+                offset: offset,
+                limit: limit,
             })
-
+            const rows = products.rows.map(product => {
+                const { dataValues } = product;
+                const url = `${req.protocol}://${req.get('host')}${req.originalUrl}/${product.productImg}`;
+                return {
+                    ...dataValues,
+                    url
+                }
+            })
+            
+            let countByCategory = await db.Product.findAll({
+                attributes: [
+                    "categorias.id","categorias.name",
+                    [db.Sequelize.fn("COUNT", db.Sequelize.col("categorias.id")), "count"],
+                ],
+                include: [{association:"categorias"}],
+                group: ['categorias.id']
+            })
+            // countByCategory.forEach(cat => {                
+            //     const {dataValues} = cat;
+            //     //console.log("cat", cat);
+            //     console.log("dataValues", dataValues);
+            // })
+            const totalPages = Math.ceil(products.count / limit)
             let response = {
-                status: 200,
-                count: products.lenght,
-                countByCategory:{},
-                products: products.rows
-                // products → array con la colección de productos, cada uno con:
-                // id
-                // name
-                // description
-                // un array con principal relación de uno a muchos (ej:categories).
-                // detail → URL para obtener el detalle.
+                totalPages: totalPages,
+                currentPageNumber: page,
+                count: products.count,
+                countByCategory,
+                products: rows
             }
                 res.status(200).json({response})            
         } catch (error) {
+            console.log(error)
             res.status(500).json({msg:"error"})
         }
     },
     productDetail: async (req,res) => {
         try {
-            let productDetail = await db.Product.findByPk(req.params.id)
+            let productDetail = await db.Product.findByPk(req.params.id,{
+                attributes: ["id","nombre","descripcion","materiales","cantidad","precio","dimensiones","peso","categoriaId","productImg"],
+                include: [{association:"categorias"},{association:"users"}]
+            })
             
+            let productImgUrl = JSON.parse(productDetail.productImg)
+
             let response = {
-                status: 200,
-                product: {
-                    id: productDetail.id,
-                    nombre: productDetail.nombre,
-                    descripcion: productDetail.descripcion,
-                    materiales: productDetail.materiales,
-                    cantidad: productDetail.cantidad,
-                    precio: productDetail.precio,
-                    dimensiones: productDetail.dimensiones,
-                    peso: productDetail.peso,
-                    categoriaId: productDetail.categoriaId,
-                    productImg: productDetail.productImg,
-                    // una propiedad por cada campo en base.
-                    // un array por cada relación de uno a muchos (categories, colors, sizes, etc).
-                    // Una URL para la imagen del producto (para mostrar la imagen).
-                }
+                product: productDetail,
+                productImgUrl: `${req.protocol}://${req.get('host')}${req.originalUrl}/${productImgUrl}`
             }
             res.status(200).json({response})
         } catch (error) {
+            console.log(error)
             res.status(500).json({msg:"error"})
         }
     }
