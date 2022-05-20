80af0b35d18309509f4321f6a20a38d8a14b73d9
AdrianoCassano <cassano.adriano@gmail.com>
2022-05-20 00:30:35 -0300

    Fix logos de SmallCards

--- todomuebles-react/src/components/SmallCard.js
+++ todomuebles-react/src/components/SmallCard.js
@@ -12,7 +12,7 @@ function SmallCard(props){
                             <div className="h5 mb-0 font-weight-bold text-gray-800">{props.quantity}</div>
                         </div>
                         <div className="col-auto">
-                            <i className={`fas ${props.icon} fa-2x text-gray-300`}></i>
+                            <i className={`fas fa-${props.icon} fa-2x text-gray-300`}></i>
                         </div>
                     </div>
                 </div>
