ea8bb02352281b5f8dcdfc4878d0c87d1f3e5060
Restair6 <renzo.restaino@gmail.com>
2022-05-22 00:16:44 -0300

    productos sugeridos actualizado

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,13 @@
+const db = require ("../database/models")
+
 const mainController = {
     home: (req,res) => {
-        res.render("home")
+        db.Product.findAll()
+        .then(products => {
+        res.render("home", {products})
+    }).catch((error)=>{
+        console.log(error)
+    })
     },
     aboutUs: (req,res) => {
         res.render("aboutUs")
